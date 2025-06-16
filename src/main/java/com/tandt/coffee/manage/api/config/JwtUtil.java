0f6e43c2ce46c4f2b54f1853a61f7533b981300e
viettrung317 <leviettrung31072001@gmail.com>
2025-06-16 23:37:13 +0700

    add jwtConfig

--- src/main/java/com/tandt/coffee/manage/api/config/JwtUtil.java
+++ src/main/java/com/tandt/coffee/manage/api/config/JwtUtil.java
@@ -1,5 +1,52 @@
 package com.tandt.coffee.manage.api.config;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
 public class JwtUtil {
 
+	@Value("${app.jwt.secret}")
+	private String secret;
+
+	@Value("${app.jwt.expiration}")
+	private Long expiretion;
+	
+	public String extractUsername(String token) {
+	    return Jwts.parserBuilder()
+	            .setSigningKey(Keys.hmacShaKeyFor(secret.getBytes()))
+	            .build()
+	            .parseClaimsJws(token)
+	            .getBody()
+	            .getSubject();
+	}
+
+	public String generateToken(String usename) {
+		return Jwts.builder()
+				.setSubject(usename)
+				.setIssuedAt(new Date())
+				.setExpiration(new Date(System.currentTimeMillis() + expiretion))
+				.signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS512)
+				.compact();
+	}
+
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parserBuilder()
+		    .setSigningKey(secret.getBytes(StandardCharsets.UTF_8))
+		    .build()
+		    .parseClaimsJws(token);
+
+			return true;
+		} catch (Exception ex) {
+			return false;
+		}
+	}
 }
--- src/main/java/com/tandt/coffee/manage/api/config/SecurityConfig.java
+++ src/main/java/com/tandt/coffee/manage/api/config/SecurityConfig.java
@@ -1,5 +1,32 @@
 package com.tandt.coffee.manage.api.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
 public class SecurityConfig {
+
+	@Autowired
+	private JwtFilter jwtFilter;
+	
+	@SuppressWarnings("removal")
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	    http.csrf().disable()
+	        .authorizeHttpRequests()
+	        .requestMatchers("/auth/**").permitAll()
+	        .anyRequest().authenticated()
+	        .and()
+	        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+	    http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+	    return http.build();
+	}
 
 }
